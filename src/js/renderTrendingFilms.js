50371e8a5abddbbc5c0b8cce23798d5489668f83
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-11 18:48:06 +0300

    No template

--- src/js/renderTrendingFilms.js
+++ src/js/renderTrendingFilms.js
@@ -1,7 +1,24 @@
 import { getTrendingMovies } from './API/api.js';
 import jsonGenres from './API/jsonGenres.js';
-import filmCardTemplate from '../components/film_card/filmCardTemplate.js';
+
 import { getImage } from './API/api.js';
+
+const filmCardTemplate = (
+  image = 'https://thumbs.dreamstime.com/b/mission-failed-text-written-red-round-vintage-rubber-stamp-mission-failed-text-written-red-vintage-round-stamp-214766654.jpg',
+  filmName = 'Check',
+  genresOfFilm = ' Failed to load',
+  year = '2021',
+  id = 'Mistake'
+) => `<div class="film__card" id="${id}">
+      <img
+        src="${image}"
+        alt="${filmName}"
+        class="film__card-poster"
+      />
+
+      <p class="film__card-name">${filmName}</p>
+      <p class="film__card-genres">${genresOfFilm} | ${year}</p>
+    </div>`;
 
 export const fetchAndRenderTemplates = async (pageNumber = 1) => {
   renderLink = document.querySelector('body');
