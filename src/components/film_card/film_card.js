b4c35145162a7fb3ca5ab3263827ba0f8eb15e1f
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-11 15:55:01 +0300

    Bug fix

--- src/components/film_card/film_card.js
+++ src/components/film_card/film_card.js
@@ -1,7 +1,7 @@
 export default filmCardTemplate = (
   image = 'https://thumbs.dreamstime.com/b/mission-failed-text-written-red-round-vintage-rubber-stamp-mission-failed-text-written-red-vintage-round-stamp-214766654.jpg',
   filmName = 'Check',
-  genres = ' Failed to load',
+  genresOfFilm = ' Failed to load',
   year = '2021',
   id = 'Mistake'
 ) => `<div class="film__card" id="${id}">
@@ -12,5 +12,5 @@ export default filmCardTemplate = (
       />
 
       <p class="film__card-name">${filmName}</p>
-      <p class="film__card-genres">${genres} | ${year}</p>
+      <p class="film__card-genres">${genresOfFilm} | ${year}</p>
     </div>`;
