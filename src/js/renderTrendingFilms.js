f8997ddd3fa1545d4018711eb006d397f279a477
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-11 18:02:19 +0300

    Trying bug fix

--- src/js/renderTrendingFilms.js
+++ src/js/renderTrendingFilms.js
@@ -7,48 +7,48 @@ export default class RenderCards {
   constructor() {
     this.renderLink = document.querySelector('body');
 
-    this.fetchAndRenderTemplates();
+    fetchAndRenderTemplates(this.renderLink);
+  }
+}
+
+const fetchAndRenderTemplates = async renderLink => {
+  const { data } = await getTrendingMovies();
+  const { page } = data;
+  const { results } = await data;
+
+  const template = results
+    .map(({ poster_path, original_title, id, genre_ids, release_date }) => {
+      const wordGenres = genresFromId(genre_ids);
+      const date = release_date.slice(0, 4);
+      const image = getImage(poster_path);
+
+      return filmCardTemplate(image, original_title, wordGenres, date, id);
+    })
+    .join('');
+
+  const templateWithContaimer = `<div class="container"><div class="card-container">${template}</div></div> `;
+
+  renderLink.insertAdjacentHTML('beforeend', templateWithContaimer);
+  return page;
+};
+
+const genresFromId = arrId => {
+  const genres = jsonGenres;
+  const genresName = [];
+
+  for (let i = 0; i < genres.length; i++) {
+    if (genres[i].id === arrId[0]) {
+      genresName.push(genres[i].name);
+    }
+
+    if (genres[i].id === arrId[1]) {
+      genresName.push(genres[i].name);
+    }
   }
 
-  fetchAndRenderTemplates = async () => {
-    const { data } = await getTrendingMovies();
-    const { page } = data;
-    const { results } = await data;
+  if (arrId[3]) {
+    genresName.push('other');
+  }
 
-    const template = results
-      .map(({ poster_path, original_title, id, genre_ids, release_date }) => {
-        const wordGenres = this.genresFromId(genre_ids);
-        const date = release_date.slice(0, 4);
-        const image = getImage(poster_path);
-
-        return filmCardTemplate(image, original_title, wordGenres, date, id);
-      })
-      .join('');
-
-    const templateWithContaimer = `<div class="container"><div class="card-container">${template}</div></div> `;
-
-    this.renderLink.insertAdjacentHTML('beforeend', templateWithContaimer);
-    return page;
-  };
-
-  genresFromId = arrId => {
-    const genres = jsonGenres;
-    const genresName = [];
-
-    for (let i = 0; i < genres.length; i++) {
-      if (genres[i].id === arrId[0]) {
-        genresName.push(genres[i].name);
-      }
-
-      if (genres[i].id === arrId[1]) {
-        genresName.push(genres[i].name);
-      }
-    }
-
-    if (arrId[3]) {
-      genresName.push('other');
-    }
-
-    return genresName.join(', ');
-  };
-}
+  return genresName.join(', ');
+};
