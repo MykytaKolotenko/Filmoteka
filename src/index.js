3b94c2de25f711a092b284a9a65098de8aa2957c
Andrey Machula <andy.machula@gmail.com>
2022-06-12 12:43:11 +0200

    adds render main-footer-template

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import main_header_template from './js/components/main/header/main_header_template';
-
+import main_footer_template from './js/components/main/footer/main_footer_template';
 // import library_header_template from './js/components/main/library_header/library_header_template';
 
 // import fetchAndRenderTemplates from './js/renderTrendingFilms.js';
@@ -11,3 +11,6 @@ document
 // fetchAndRenderTemplates();
 
 // ____________________
+document
+  .querySelector('body')
+  .insertAdjacentHTML('afterend', main_footer_template());
