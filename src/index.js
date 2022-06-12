8c2fa5a92dac0d6393df7fb0c36df3bd94583ad9
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-12 11:38:05 +0300

    header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-// import main_header_template from './js/components/main/header/main_header_template';
+import main_header_template from './js/components/main/header/main_header_template';
 
 // import library_header_template from './js/components/main/library_header/library_header_template';
 
 // // import fetchAndRenderTemplates from './js/renderTrendingFilms.js';
 
-// document
-//   .querySelector('body')
-//   .insertAdjacentHTML('beforeend', main_header_template());
+document
+  .querySelector('body')
+  .insertAdjacentHTML('beforeend', main_header_template());
 
 // // fetchAndRenderTemplates();
 
