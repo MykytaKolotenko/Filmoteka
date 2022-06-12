10ac57dd77b325b10b2b6169502a94e05670beb9
Andrey Machula <andy.machula@gmail.com>
2022-06-12 12:41:48 +0200

    adds main-footer-template

---
+++ src/js/components/main/footer/main_footer_template.js
@@ -0,0 +1,16 @@
+import pictureHeart from '../../../../images/heart.svg';
+
+export default function mainFooterTemplate() {
+  return `<footer class="footer">
+      <div class="container footer-container">
+        <p class="footer-text">
+          &copy; 2022 &#124; All Rights Reserved &#124; Developed with
+          <svg class="footer-text-icon" width="17px" height="16px">
+            <use href="${pictureHeart}"></use>
+          </svg>
+          by
+          <button class="modal-open-btn" type="button">GoIT Students</button>
+        </p>
+      </div>
+    </footer>`;
+}
