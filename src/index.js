b4463e26056f68d8626a4c85d303e8718027c5fe
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-12 12:12:13 +0300

    only header

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import main_header_template from './js/components/main/header/main_header_templa
 
 // import library_header_template from './js/components/main/library_header/library_header_template';
 
-import fetchAndRenderTemplates from './js/renderTrendingFilms.js';
+// import fetchAndRenderTemplates from './js/renderTrendingFilms.js';
 
 document
   .querySelector('body')
