4288060547ec7d4897018d191715ee1c1917ed28
Yana Mironyak <yanika.mironyak@gmail.com>
2022-06-14 02:21:16 +0300

    Add modal css

--- src/index.js
+++ src/index.js
@@ -1,47 +1,4 @@
 import RenderFilm from './js/RenderFilm-Class';
-
-import library_header_template from './js/components/main/library_header/library_header_template';
-
-import fetchAndRenderTemplates from './js/renderTrendingFilms.js';
-
-import { getMovie, getImage } from './js/API/api';
-
-document
-  .querySelector('body')
-  .insertAdjacentHTML('beforeend', main_header_template());
-
-waitRender();
-
-async function waitRender() {
-  await fetchAndRenderTemplates();
-  
-  const refs = {
-    filmCard: document.querySelector('.card-container'),
-    backdrop: document.querySelector('.backdrop'),
-    modalBody: document.querySelector('.modal__content'),
-  }
-
-  refs.filmCard.addEventListener('click', onOpenFilmCard);  
-
-  async function onOpenFilmCard(evt) {
-  refs.backdrop.classList.remove('is-hidden');
-
-    const movieId = evt.target.parentNode.id;
-
-    const data = await getMovie(movieId);
-
-    const moviePath = data.data.backdrop_path;
-
-    const image = await getImage(moviePath);
-
-    return refs.modalBody.innerHTML = `
-    <img src=${image}>
-    <p>${data.data.title}</p>
-    <p>${data.data.vote_average}</p>
-    <p>${data.data.popularity}</p>
-    <p>${data.data.original_title}</p>
-    <p>${data.data.overview}</p>` 
-  }
-};
+import filmModal from './js/components/filmModal/filmModal';
 
 new RenderFilm();
--- src/js/components/filmModal/filmModal.js
+++ src/js/components/filmModal/filmModal.js
@@ -1,12 +1,50 @@
-const refs = {
+import { getMovie, getImage } from '../../API/api';
 
-    filmCardRef: document.querySelector('.film__card'),
+const refs = {
+    cardContainer: document.querySelector('main'),
+    backdrop: document.querySelector('.backdrop'),
+    modalBody: document.querySelector('.modal__content'),
+    closeBtn: document.querySelector('.modal__close-btn')
+}
+  
+refs.cardContainer.addEventListener('click', openFilmCard);
+refs.closeBtn.addEventListener('click', closeModal);
+
+async function openFilmCard(evt) {
     
+    const movieId = evt.target.parentNode.id;
+    const data = await getMovie(movieId);
+    const moviePath = data.data.poster_path;
+    const movieImage = await getImage(moviePath);
+    const targetTag = evt.target.parentNode.nodeName;
+
+    const movieGenres = Object.values(data.data.genres).map(genres => genres.name).join(' ')
+
+    console.log(data.data)
+
+    if (targetTag !== "LI") {
+        return; 
+    }
+    refs.backdrop.classList.remove('is-hidden');
+
+    return refs.modalBody.innerHTML = 
+        `<div class="modal__image-wrapper">
+        <img src=${movieImage} class="modal__image" >
+        </div>
+        <div class="modal__film-info">
+        <p class="modal__title">${data.data.title}</p>
+        
+        <p>${data.data.vote_average}/${data.data.vote_count}</p>
+        <p>${data.data.popularity}</p>
+        <p>${data.data.original_title}</p>
+        <p>${movieGenres}</p>
+        
+        <div class="modal__overview">
+        <p class="modal__overview-title">About</p>
+        <p class="modal__overview-text">${data.data.overview}</p>
+        </div></div>`
 }
 
-refs.filmCardRef.addEventListener('click', onOpenFilmCard);
-
-function onOpenFilmCard(evt) {
-    evt.preventDefault;
-    document.body.classList.add('show__modal');
+function closeModal() {
+    refs.backdrop.classList.add('is-hidden');
 }
