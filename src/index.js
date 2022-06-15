54169614b6f876cf493afe154fc76686debc9a3d
Yana Mironyak <yanika.mironyak@gmail.com>
2022-06-15 03:11:23 +0300

    Add modal Class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import mainPage from './js/mainPageClass.js';
 import filmModal from './js/components/filmModal/filmModal';
+import ModalAPI from './js/components/filmModal/filmModal';
 
 new mainPage();
 
+new ModalAPI();
