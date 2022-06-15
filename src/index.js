e6189ab0bbc6460e0510b771165d6e41bd98b73c
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-06-15 20:30:25 +0300

    height correction and delete template

--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
 import mainPage from './js/mainPageClass.js';
 new mainPage();
-// class BtnTop{
-//     constructor({main, btnToTop}) {
-//         //  this.main = document.querySelector('main');
-//         // this.btnToTop = document.querySelector('.to-top');
-//     }
-//     const elmToTop = ` <div class="to-top"> <button class="btn-to-top" data-main="up"></button></div>`;
-//     this.main.insertAdjacentHTML("afterbegin", elmToTop);
-    
-//     this.btnToTop.addEventListener('click', function (e) {
-//      e.preventDefault();
-//      window.scrollTo({
-//        top: 0,
-//        behavior: 'smooth',
-//      });
-//     });
-    
-    
-//     setInterval(function displayBtnScrollTop() {
-//      if (window.scrollY >= 500) {
-//        btnToTop.style.display = 'inline-block';
-//      } else {
-//        btnToTop.style.display = 'none';
-//      }
-//     }, 250);
-// }
+
 
 
 
