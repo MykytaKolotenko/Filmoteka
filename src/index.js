26eea10c4643d9c2127c9843321638a7a4535437
Yana Mironyak <yanika.mironyak@gmail.com>
2022-06-17 13:42:50 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import mainPage from './js/mainPageClass.js';
 import filmModal from './js/components/filmModal/filmModal';
 import ModalAPI from './js/components/filmModal/filmModal';
-import ModalFooter from './js/components/main/footerModal/footerModal';
 
 new mainPage();
-new ModalAPI();
 
 new ModalAPI();
-
-new ModalFooter();
 
