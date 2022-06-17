0b7f298a356947633f5cf722608a74374fac0ae1
Andrey Machula <andy.machula@gmail.com>
2022-06-17 10:07:14 +0200

    Import ModalFooter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import mainPage from './js/mainPageClass.js';
 import filmModal from './js/components/filmModal/filmModal';
 import ModalAPI from './js/components/filmModal/filmModal';
+import ModalFooter from './js/components/main/footerModal/footerModal';
 
 new mainPage();
 
 new ModalAPI();
-тщ;
+
+new ModalFooter();
