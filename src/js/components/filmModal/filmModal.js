976015de96e6a07e65f24840ab510311b81c7124
Yana Mironyak <yanika.mironyak@gmail.com>
2022-06-17 13:10:13 +0300

    Fix bugs

--- src/js/components/filmModal/filmModal.js
+++ src/js/components/filmModal/filmModal.js
@@ -38,7 +38,11 @@ export default class ModalAPI {
         <div class="modal__film-info">
         <div class="film-info__wrapp">
         <p class="film-info__property">Vote / Votes</p>
-        <p class="film-info__value">${data.data.vote_average} / ${data.data.vote_count}</p>
+        <div class="film-info__value">
+        <div class="film-info__value--orange">${data.data.vote_average}</div>
+        <div class="film-info__value--slash"> / </div>
+        <div class="film-info__value--grey">${data.data.vote_count}</div>
+        </div>
         </div>
         <div class="film-info__wrapp">
         <p class="film-info__property">Popularity</p>
