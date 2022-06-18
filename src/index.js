88c157899717c069e3afc031a9419180fcacfc2b
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-18 20:40:41 +0300

    BugFix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import mainPage from './js/mainPageClass.js';
-import filmModal from './js/components/filmModal/filmModal';
-import ModalAPI from './js/components/filmModal/filmModal';
+npmimport ModalAPI from './js/components/filmModal/filmModal';
 import ModalFooter from './js/components/main/footerModal/footerModal';
 
+import Loader from './js/components/loader/loader-square';
 import libraryPage from './js/libraryPageClass.js';
-import Loader from './js/components/loader/loader-square'
-
-import libraryPage from './libraryPageClass.js';
-
 
 new libraryPage();
 // new mainPage();
 
 new ModalAPI();
 
-
 new ModalFooter();
 
-
 new Loader();
-
-
