2078f1f4f757272c80f58b33df4a5abc208909f2
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-06-20 15:45:05 +0300

    fix

--- src/js/components/main/teamCard/jsonTeam.js
+++ src/js/components/main/teamCard/jsonTeam.js
@@ -1,94 +1,149 @@
+import nik from '../../../../images/team/nikita.jpg';
+import andy from '../../../../images/team/andy.jpg';
+import yana from '../../../../images/team/yana.jpg';
+import dim from '../../../../images/team/dima.jpg';
+import alexk from '../../../../images/team/alex_k.jpg';
+import alex from '../../../../images/team/alex.jpg';
+import sofia from '../../../../images/team/sofia.jpg';
+import alexs from '../../../../images/team/alex_s.jpg';
+import mykola from '../../../../images/team/kolya.jpg';
+import slava from '../../../../images/team/slava.jpg';
+
+import git from '../../../../images/icons_link/github.svg';
+import tel from '../../../../images/icons_link/telegram.svg';
+import lin from '../../../../images/icons_link/linkedin.svg';
+
 const developers = [
   {
-    "id": 0,
-    "name": "Nikita Kolotenko",
-    "title": "Team Lead",
-    "description": "",
-    "image": "nikita.7eb56676.jpg?1655653762229",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 0,
+    name: 'Nikita Kolotenko',
+    title: 'Team Lead',
+    description: '',
+    image: nik,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 1,
-    "name": "Andrii Machula",
-    "title": "Scrum master",
-    "description": "",
-    "image": "andy.d371e58d.jpg?1655654007933",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 1,
+    name: 'Andrii Machula',
+    title: 'Scrum master',
+    description: '',
+    image: andy,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 2,
-    "name": "Yana Mironyak",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "yana.a9e0127e.jpg?1655654173414",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 2,
+    name: 'Yana Mironyak',
+    title: 'Front-end developer',
+    description: '',
+    image: yana,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 3,
-    "name": "Dmytro Harnazhenko",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "dima.da1003bf.jpg?1655654210677",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 3,
+    name: 'Dmytro Harnazhenko',
+    title: 'Front-end developer',
+    description: '',
+    image: dim,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 4,
-    "name": "Alexander Kotenko",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "alex_k.1d06bb1a.jpg?1655654250541",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 4,
+    name: 'Alexander Kotenko',
+    title: 'Front-end developer',
+    description: '',
+    image: alexk,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 5,
-    "name": "Alexey Konstantinov",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "alex.45b4e3b0.jpg?1655654297746",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 5,
+    name: 'Alexey Konstantinov',
+    title: 'Front-end developer',
+    description: '',
+    image: alex,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 6,
-    "name": "Sofia Gromovaya",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "sofia.ef54a800.jpg?1655654336345",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 6,
+    name: 'Sofia Gromovaya',
+    title: 'Front-end developer',
+    description: '',
+    image: sofia,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 7,
-    "name": "Aleksandr Sidorskiy",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "alex_s.51903dc5.jpg?1655654370867",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 7,
+    name: 'Aleksandr Sidorskiy',
+    title: 'Front-end developer',
+    description: '',
+    image: alexs,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 8,
-    "name": "Mykola Shchyrba",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "kolya.b52b7f51.jpg?1655654413466",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 8,
+    name: 'Mykola Shchyrba',
+    title: 'Front-end developer',
+    description: '',
+    image: mykola,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
   {
-    "id": 9,
-    "name": "Slava Nadtochiy",
-    "title": "Front-end developer",
-    "description": "",
-    "image": "slava.60a4b980.jpg?1655654452532",
-    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
-    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+    id: 9,
+    name: 'Slava Nadtochiy',
+    title: 'Front-end developer',
+    description: '',
+    image: slava,
+    links: [
+      'https://github.com',
+      'https://web.telegram.org/',
+      'https://www.linkedin.com/',
+    ],
+    icons: [git, tel, lin],
   },
 ];
 
-export default developers
+export default developers;
