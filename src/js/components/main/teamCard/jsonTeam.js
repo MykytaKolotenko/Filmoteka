6793529145e66ec05d8a59e87f1a6b4fef441098
Andrey Machula <andy.machula@gmail.com>
2022-06-20 11:06:52 +0200

    Update jsonTeam.js

--- src/js/components/main/teamCard/jsonTeam.js
+++ src/js/components/main/teamCard/jsonTeam.js
@@ -1,84 +1,93 @@
 const developers = [
   {
     "id": 0,
-    "name": "Student-1",
+    "name": "Nikita Kolotenko",
     "title": "Team Lead",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "nikita.7eb56676.jpg?1655653762229",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 1,
-    "name": "Student-2",
+    "name": "Andrii Machula",
     "title": "Scrum master",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "andy.d371e58d.jpg?1655654007933",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 2,
-    "name": "Student-3",
+    "name": "Yana Mironyak",
     "title": "Front-end developer",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "yana.a9e0127e.jpg?1655654173414",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 3,
-    "name": "Student-4",
+    "name": "Dmytro Harnazhenko",
     "title": "Front-end developer",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "dima.da1003bf.jpg?1655654210677",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 4,
-    "name": "Student-5",
+    "name": "Alexander Kotenko",
     "title": "Front-end developer",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "alex_k.1d06bb1a.jpg?1655654250541",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 5,
-    "name": "Student-6",
+    "name": "Alexey Konstantinov",
     "title": "Front-end developer",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "alex.45b4e3b0.jpg?1655654297746",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 6,
-    "name": "Student-7",
+    "name": "Sofia Gromovaya",
     "title": "Front-end developer",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "sofia.ef54a800.jpg?1655654336345",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 7,
-    "name": "Student-8",
+    "name": "Aleksandr Sidorskiy",
     "title": "Front-end developer",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "alex_s.51903dc5.jpg?1655654370867",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
   {
     "id": 8,
-    "name": "Student-9",
+    "name": "Mykola Shchyrba",
     "title": "Front-end developer",
     "description": "",
-    "image": "../../../../images/no-thumb_300.jpg",
-    "links": ["https://github.com", "https://www.facebook.com/", "https://www.linkedin.com/"],
-    "icon": ["../../../../images/no-thumb_300.jpg"]
+    "image": "kolya.b52b7f51.jpg?1655654413466",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
+  },
+  {
+    "id": 9,
+    "name": "Slava Nadtochiy",
+    "title": "Front-end developer",
+    "description": "",
+    "image": "slava.60a4b980.jpg?1655654452532",
+    "links": ["https://github.com", "https://web.telegram.org/", "https://www.linkedin.com/"],
+    "icons": ["github.2c89f2ec.svg?1655660259957", "telegram.152ac010.svg?1655660568824", "linkedin.75f25008.svg?1655660652771"]
   },
 ];
 
