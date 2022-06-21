a7935b79a6c55cf404aa40892122cc1af1401730
Andrey Machula <andy.machula@gmail.com>
2022-06-21 02:18:06 +0200

    Update main_footer_template.js

--- src/js/components/main/footer/main_footer_template.js
+++ src/js/components/main/footer/main_footer_template.js
@@ -1,15 +1,17 @@
 import pictureHeart from '../../../../images/heart.svg';
 
-export default function mainFooterTemplate() {
+const mainFooterTemplate = () => {
   return `
-      <div class="footer-box"> 
+      <div class="footer__box"> 
       <div class="container footer__container">
         <p class="footer__text">
           &copy; 2022 &#124; All Rights Reserved &#124; Developed with
-           <img class="heart" src="${pictureHeart}" width="14" height="13" />
+           <img class="footer__heart-img" src="${pictureHeart}" width="14" height="13" />
           by
           <button class="footer__modal-open-btn" type="button">GoIT Students</button>
         </p>
       </div>
     </div>`;
 }
+
+export default mainFooterTemplate
