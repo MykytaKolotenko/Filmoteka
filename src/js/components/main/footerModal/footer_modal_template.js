c459d95abb2a7dd6658019d45f3229631f07b748
Andrey Machula <andy.machula@gmail.com>
2022-06-22 04:49:04 +0200

    Connects image via svg

--- src/js/components/main/footerModal/footer_modal_template.js
+++ src/js/components/main/footerModal/footer_modal_template.js
@@ -1,14 +1,12 @@
+import svg from '../../../../images/symbol-defs.svg';
+
 const footerModalTemplate = () => {
   return `<div class="footer__modal js-footer__modal">
   <div class="footer__modal-window js-footer__modal-window">
     <button class="footer__modal-close-btn">
-      <img
-        class="footer__modal-close-btn-icon"
-        src="http:close.b195aaf1.svg?1655648453393"
-        alt=""
-        width="26px"
-        height="26px"
-      />
+      <svg class="footer__modal-close-btn-icon" alt="close"  width="26px" height="26px">
+        <use href="${svg + '#icon-close'}"></use>
+      </svg>   
     </button>
     <h2 class="modal__team-title">Our Team</h2>
     <ul class="modal__team-list js-team-list"></ul>
