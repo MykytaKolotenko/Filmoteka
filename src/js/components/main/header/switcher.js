ad737751b8fe77330e08f13671d0127c3ff7b8e5
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2022-06-22 16:35:49 +0300

    dark theme + page title + other scss changes

---
+++ src/js/components/main/header/switcher.js
@@ -0,0 +1,68 @@
+export default class Switcher {
+
+  constructor() { 
+    this.refs = {
+      checkboxBtn : document.querySelector('#theme-switch-toggle'),
+      body: document.querySelector('body'),
+      modalFilm: document.querySelector('.modal'),
+      modal: document.querySelector('.footer__modal-window'),
+      footer: document.querySelector('.footer'),
+    }
+    this.theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+  this.changeTheme ()
+  }
+
+  changeTheme() {
+  
+    const { LIGHT, DARK } = this.theme;
+  
+this.refs.checkboxBtn.addEventListener('change', onChangeTheme.bind(this));
+
+function onChangeTheme(e) {
+  const theme = e.target.checked;
+  if (theme) {
+    this.refs.body.classList.add(DARK);
+    this.refs.modalFilm.classList.add(DARK);
+    this.refs.modal.classList.add(DARK);
+    this.refs.footer.classList.add(DARK);
+    
+    this.refs.body.classList.remove(LIGHT);
+    this.refs.modalFilm.classList.remove(LIGHT);
+    this.refs.modal.classList.remove(LIGHT);
+    this.refs.footer.classList.remove(LIGHT);
+  
+    localStorage.setItem('theme', DARK);
+  } else {
+    this.refs.body.classList.add(LIGHT);
+    this.refs.modalFilm.classList.add(LIGHT);
+    this.refs.modal.classList.add(LIGHT);
+    this.refs.footer.classList.add(LIGHT);
+  
+    this.refs.body.classList.remove(DARK);
+    this.refs.modalFilm.classList.remove(DARK);
+    this.refs.modal.classList.remove(DARK);
+    this.refs.footer.classList.remove(DARK);
+
+    localStorage.setItem('theme', LIGHT);
+  }
+}
+
+let theme = localStorage.getItem('theme');
+
+if (!theme) {
+  theme = LIGHT;
+  localStorage.setItem('theme', theme);
+}
+
+this.refs.body.classList.add(theme);
+this.refs.modalFilm.classList.add(theme);
+this.refs.footer.classList.add(theme);
+this.refs.modal.classList.add(theme);
+
+    
+this.refs.checkboxBtn.checked = theme === LIGHT ? false : true;
+  }
+}
--- src/js/fetchAndRenderClass.js
+++ src/js/fetchAndRenderClass.js
@@ -229,4 +229,62 @@ export default class fetchAndRender {
         .classList.remove('visually-hidden');
     }
   }
+   changeTheme() {
+    const checkboxBtn = document.querySelector('#theme-switch-toggle');
+    const body = document.querySelector('body');
+    const modalFilm = document.querySelector('.modal');
+    const modal = document.querySelector('.footer__modal-window');
+    const footer = document.querySelector('.footer');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const { LIGHT, DARK } = Theme;
+
+checkboxBtn.addEventListener('change', changeTheme);
+
+function changeTheme(e) {
+  const theme = e.target.checked;
+  if (theme) {
+    body.classList.add(DARK);
+    modalFilm.classList.add(DARK);
+    footer__modal - window.classList.add(DARK);
+    footer.classList.add(DARK);
+
+    body.classList.remove(LIGHT);
+    modalFilm.classList.remove(LIGHT);
+    footer__modal - window.classList.remove(LIGHT);
+    footer.classList.remove(LIGHT);
+    
+    localStorage.setItem('theme', DARK);
+  } else {
+    body.classList.add(LIGHT);
+    modalFilm.classList.add(LIGHT);
+    footer__modal - window.classList.add(LIGHT);
+    footer.classList.add(LIGHT);
+
+    body.classList.remove(DARK);
+    modalFilm.classList.remove(DARK);
+    footer__modal - window.classList.remove(DARK);
+    footer.classList.remove(DARK);
+
+    localStorage.setItem('theme', LIGHT);
+  }
+}
+
+let theme = localStorage.getItem('theme');
+
+if (!theme) {
+  theme = LIGHT;
+  localStorage.setItem('theme', theme);
+}
+body.classList.add(theme);
+footer__modal-window.classList.add(theme);
+modalFilm.classList.add(theme);
+footer.classList.add(theme);
+
+checkboxBtn.checked = theme === LIGHT ? false : true;
+  } 
 }
